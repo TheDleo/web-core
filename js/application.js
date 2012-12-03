f8edeeb79b9f9b88f6f80feacedb6709cd0eb9f6
Jason Sykes <jasonsykes@dreamfactory.com>
2012-12-03 11:30:50 -0500

    initial commit for web-core

---
+++ js/application.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: jasonsykes
+ * Date: 11/28/12
+ * Time: 1:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+DF = {};
+DF.Application = function (config) {
+    this.config = config;
+    this.actions = Actions;
+    this.actions.doLayout();
+};
+
+
+
