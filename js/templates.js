2c80812c0d30ff205c3f0600a2448da7aecbadbf
Jason Sykes <jasonsykes@dreamfactory.com>
2013-01-14 15:13:58 -0500

    new thumbnail template for initial launchpad view

--- js/templates.js
+++ js/templates.js
@@ -43,29 +43,40 @@ Templates = function () {
         '<a class="btn btn-warning dropdown-toggle" data-toggle="dropdown" href="#">' +
         'Applications' +
         '<span class="caret"></span>' +
-    '</a>' +
+        '</a>' +
         '<ul class="dropdown-menu">' +
         '{{#record}}' +
         '<li><a data-target="#" href="#" onclick = "Actions.showApp(\'{{fields.name}}\',\'{{fields.url}}\')">{{fields.label}}</a></li>' +
         '{{/record}}' +
-    '</ul>' +
+        '</ul>' +
         '</div>';
     this.navBarDropDownTemplate = '<ul class="nav">' +
         '<li class="dropdown">' +
         '<a data-target="#" href="#" class="dropdown-toggle" data-toggle="dropdown">' +
         'Applications' +
-           ' <b class="caret"></b>' +
+        ' <b class="caret"></b>' +
         '</a>' +
         '<ul class="dropdown-menu">' +
         '{{#Applications}}' +
         '<li><a data-target="#" href="#" onclick = "Actions.showApp(\'{{name}}\',\'{{url}}\',\'{{is_url_external}}\')">{{label}}</a></li>' +
         '{{/Applications}}' +
-    '</ul>' +
-    '</li>' +
-    '</ul>';
+        '</ul>' +
+        '</li>' +
+        '</ul>';
     this.userInfoTemplate = '<div class="btn-group"><a id="dfProfileLnk" class="btn btn-primary" title="Change Your Profile">' +
         '<i class="icon-user"></i>&nbsp;{{full_name}}</a><a id="dfPasswordLnk" class="btn btn-info" title="Change Your Password"><i class="icon-key"></i></a>' +
         '<a id="dfSignOutLink" class="btn btn-info" title="End Your Session Now"><i class="icon-signout"></i></a></div>';
+    this.appIconTemplate = '{{#Applications}}' +
+        '<div class="media" style="margin:0px;" onclick = "Actions.showApp(\'{{name}}\',\'{{url}}\',\'{{is_url_external}}\')">' +
+        '<span class=" well warning pull-left" data-target="#" href="#">' +
+        '<i class="icon-cogs icon-2x"></i>' +
+        '</span>' +
+        '<div class="media-body">' +
+        '<h4 class="media-heading">{{label}}</h4>' +
+        '{{description}}' +
+        '</div>' +
+        '</div>' +
+        '{{/Applications}}';
     this.loadTemplate = function (template, data, renderTo) {
         var processTpl;
         processTpl = Mustache.to_html(template, data);
