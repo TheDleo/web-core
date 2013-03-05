d6dfbb0b91eb2b94fda5128821f85ac5b9d94b05
Todd <todd@dreamfactory.com>
2013-03-05 09:14:47 -0500

    General purpose error parser for top-level errors.

--- js/df-err.js
+++ js/df-err.js
@@ -1,56 +1,39 @@
-function parseErrors(data,error) {
-    if(!(data instanceof String)) {
-        if(data.records) data = data.records;
-        if(data.record) {
-            var errs = makeForFaults(data.record);
-            if(errs.length > 0) {
-                error(errs,data);
-                return true;
-            }
-        } else if(data.file || data.folder) {
-            var errs = makeForFaults(data.folder);
-            makeForFaults(data.file,errs);
-            if(errs.length > 0) {
-                error(errs,data);
-                return true;
-            }
-        } else if(data.fault) {
-            error([makeErrorReport(data.fault)],data);
-            return true;
-        }
-    }
+function alertErr(response) {
+
+    alert(getErrorString(response));
 }
 
-/**
- *
- * @param data
- * @returns {Array}
- */
-function makeForFaults(data,errs) {
-    if(!errs) errs = [];
-    for(var i in data) {
-        if(data[i].fault) {
-            errs[errs.length] = makeErrorReport(data[i].fault);
+function getErrorString(response) {
+
+    var code = null;
+    var value = null;
+
+    if (response) {
+        value = response.status;
+        if (value) {
+            code = value;
+        }
+        value = response.responseText;
+        if (value && value != '') {
+            try {
+                var result = JSON.parse(value);
+                if (result) {
+                    value = result.error[0].message;
+                    if (value && value != '') {
+                        return value;
+                    }
+                    value = result.error[0].code;
+                    if (value) {
+                        code = value;
+                    }
+                }
+            } catch(e) {
+
+            }
+        }
+        if (code !== null) {
+            return 'Server returned error code ' + code + '.';
         }
     }
-    return errs;
-}
-
-/**
- *
- * @param json
- * @returns {String}
- */
-function makeErrorReport(json) {
-    var str = "";
-
-    if(json.faultCode) {
-        str += json.faultCode+": ";
-    }
-
-    if(json.faultString) {
-        str += json.faultString;
-    }
-
-    return str;
+    return 'Server returned an unknown error.';
 }
