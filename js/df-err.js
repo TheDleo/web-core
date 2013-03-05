a19af8f8625bd35839343063d974ea9305f6fe26
Todd <todd@dreamfactory.com>
2013-03-05 10:42:23 -0500

    Support response.responseText or response.data.

--- js/df-err.js
+++ js/df-err.js
@@ -7,6 +7,7 @@ function getErrorString(response) {
 
     var code = null;
     var value = null;
+    var result = null;
 
     if (response) {
         value = response.status;
@@ -16,19 +17,21 @@ function getErrorString(response) {
         value = response.responseText;
         if (value && value != '') {
             try {
-                var result = JSON.parse(value);
-                if (result) {
-                    value = result.error[0].message;
-                    if (value && value != '') {
-                        return value;
-                    }
-                    value = result.error[0].code;
-                    if (value) {
-                        code = value;
-                    }
-                }
+                result = JSON.parse(value);
             } catch(e) {
 
+            }
+        } else {
+            result = response.data;
+        }
+        if (result) {
+            value = xml2text(result.error[0].message);
+            if (value && value != '') {
+                return value;
+            }
+            value = result.error[0].code;
+            if (value) {
+                code = value;
             }
         }
         if (code !== null) {
@@ -36,4 +39,14 @@ function getErrorString(response) {
         }
     }
     return 'Server returned an unknown error.';
+}
+
+function text2xml(value) {
+
+    return value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/\'/g, '&apos;');
+}
+
+function xml2text(value) {
+
+    return value.replace(/&quot;/g, '"').replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&').replace(/&apos;/g, '\''); ;
 }
